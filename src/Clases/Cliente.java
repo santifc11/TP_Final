3ff3a297bd43ad34c3b7377218bda0f40eecaf55
FelipeCliment06 <felipe-climent@estudiante.mdp.utn.edu.ar>
2024-11-16 20:01:52 -0300

    contructor clase cliente
    complete la clase cliente

--- src/Clases/Cliente.java
+++ src/Clases/Cliente.java
@@ -1,6 +1,8 @@
 package Clases;
 
+import java.util.Objects;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Cliente extends Usuario{
     private String dni;
@@ -9,8 +11,94 @@ public class Cliente extends Usuario{
     private String telefono;
     private String mail;
     private boolean comparte;
+    Set<Cliente> listaclientes;
 
-    Set<Reserva> historialReserva;
+    public Cliente(String usuario, String contrasenia, String dni, String nombre, String apellido, String telefono, String mail, boolean comparte) {
+        super(usuario, contrasenia);
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.mail = mail;
+        this.comparte = comparte;
+        this.listaclientes = new TreeSet<>();
 
-    
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public boolean isComparte() {
+        return comparte;
+    }
+
+    public void setComparte(boolean comparte) {
+        this.comparte = comparte;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(dni, cliente.dni);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), dni);
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "dni='" + dni + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", mail='" + mail + '\'' +
+                ", comparte=" + comparte +
+                ", listaclientes=" + listaclientes +
+                "} " + super.toString();
+    }
+
+
 }
